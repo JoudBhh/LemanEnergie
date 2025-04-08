b7e6325e95cae265a63d3e30bad9d742256f1fbb
Joud <joud.hajhmida@gmail.com>
2025-04-08 20:06:30 +0100

    config change

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 import mdx from '@astrojs/mdx';
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
