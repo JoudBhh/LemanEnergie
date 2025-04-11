29b995ec694b1f1aba588ea0446c95fc6dc3ee56
Joud <joud.hajhmida@gmail.com>
2025-04-11 14:39:23 +0100

    add nouveau article

--- src/utils/imgMap.ts
+++ src/utils/imgMap.ts
@@ -4,6 +4,8 @@ import art3img1 from '../images/art3-img1.jpg';
 import art4img1 from '../images/art4-img1.jpg';
 import art5img1 from '../images/art5-img1.jpg';
 import art6img1 from '../images/art6-img1.jpg';
+import art7img1 from '../images/art7-img1.jpg';
+import art7img2 from '../images/art7-img2.jpg';
 import image11 from '../images/image11.jpg';
 import image12 from '../images/signes-indiquant-que-vous-avez-besoin-dun-adoucisseur-eau-image.jpg';
 
@@ -15,6 +17,8 @@ export const imageMap = {
   'art4-img1.jpg': art4img1,
   'art5-img1.jpg': art5img1,
   'art6-img1.jpg': art6img1,
+  'art7-img1.jpg': art7img1,
+  'art7-img2.jpg': art7img2,
   'image11.jpg': image11,
   'image12.jpg': image12,
 
